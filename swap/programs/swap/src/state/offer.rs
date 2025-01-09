08773a3ff8b79a5f9c8b5a0033bf73c9fc41f71e
xiaxianlin <dreamtimefly@163.com>
2025-01-09 11:03:34 +0800

    画图

--- swap/programs/swap/src/state/offer.rs
+++ swap/programs/swap/src/state/offer.rs
@@ -4,9 +4,13 @@ use anchor_lang::prelude::*;
 #[derive(InitSpace)]
 pub struct Offer {
     pub id: u64,
+    /// 制单人
     pub maker: Pubkey,
+    /// 持有的代币
     pub token_mint_a: Pubkey,
+    /// 想要的代币
     pub token_mint_b: Pubkey,
+    /// 想要的代币数量
     pub token_b_wanted_amount: u64,
     pub bump: u8, 
 }
